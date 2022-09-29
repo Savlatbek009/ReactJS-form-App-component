9276cc648c9478d877c474551fbd99e8e593eed4
Savlatbek Abdullayev <109302557+Savlatbek009@users.noreply.github.com>
2022-09-29 05:23:26 -0600

    React form app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+import { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
+  }
+}
